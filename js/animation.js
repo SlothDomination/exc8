2869ac79e9330a00553ae39a62bd07da61ad3dbe
SlothDomination <e1630725@cmaisonneuve.qc.ca>
2019-02-26 12:57:02 -0500

    ajouter animation du A de site-title de la classe site-branding

--- js/animation.js
+++ js/animation.js
@@ -3,9 +3,12 @@ window.addEventListener('load', function(){//ajoute le js dès le depart.
     //console.log('YASSSS');
 
     let animSiteHeader = document.getElementById("masthead");
-    animSiteHeader.classList.add("Anim_site-header");
+    let animSiteTitle = document.querySelector("header > div > div > p > a");
 
-     console.log(animSiteHeader);
+    animSiteHeader.classList.add("Anim_site-header");
+    animSiteTitle.classList.add("Anim_site-title");
+
+     //console.log(animSiteHeader);
 
   
 })
