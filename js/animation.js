0c1ad4172f4638af161bcf866977ef42f202e034
SlothDomination <e1630725@cmaisonneuve.qc.ca>
2019-02-26 11:55:52 -0500

    animation site-header activer des lancement page

--- js/animation.js
+++ js/animation.js
@@ -3,7 +3,7 @@ window.addEventListener('load', function(){//ajoute le js dès le depart.
     //console.log('YASSSS');
 
     let animSiteHeader = document.getElementById("masthead");
-    animSiteHeader.classList.add(".Anim_site-header");
+    animSiteHeader.classList.add("Anim_site-header");
 
      console.log(animSiteHeader);
 
